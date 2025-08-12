dfafd140f025f0b83459e0b1b33ba76b03f58ae5
mohammadsofan <nassersofan@gmail.com>
2025-08-12 20:03:59 +0300

    feat: add messagesender, messagereciver and implement rabbitmq message broker

---
+++ MessageReciver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace MessageReciver
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672"),
+                ClientProvidedName = "MessageReciverClient"
+            };
+            var connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+            var exchangeName = "test_exchange";
+            var queueName = "test_queue";
+            var routeKey = "test_route";
+            await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct, false, false, null);
+            await channel.QueueDeclareAsync(queueName, false, false, false, null);
+            await channel.QueueBindAsync(queueName, exchangeName, routeKey, null);
+            //what Qos means: Quality of Service (QoS) settings control how many messages the consumer can process at once.
+            await channel.BasicQosAsync(0, 1, false);
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync+= async(sender,content)=>
+            {
+                Task.Delay(3000).Wait();
+                var body = content.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message received: {message}");
+                await channel.BasicAckAsync(content.DeliveryTag, false);
+            };
+            string consumerTag = await channel.BasicConsumeAsync(queueName, false, consumer);
+            Console.ReadLine();
+            await channel.BasicCancelAsync(consumerTag);
+            await channel.CloseAsync();
+            await connection.CloseAsync();
+        }
+    }
+}
---
+++ MessageSender/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+namespace MessageSender
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "MessageSenderClient";
+            IConnection connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+            var exchange = "test_exchange";
+            var queueName = "test_queue";
+            var routekey = "test_route";
+            await channel.ExchangeDeclareAsync(exchange, ExchangeType.Direct, false,false,null);
+            await channel.QueueDeclareAsync(queueName, false, false, false, null);
+            await channel.QueueBindAsync(queueName, exchange, routekey, null);
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Delay(1000).Wait(); 
+                var message = $"Message {i + 1}";
+                var body = Encoding.UTF8.GetBytes(message);
+                await channel.BasicPublishAsync(exchange, routekey, body);
+                Console.WriteLine($"Message sent: {message}");
+            }
+            await channel.CloseAsync();
+            await connection.CloseAsync();
+            Console.WriteLine("Connection closed.");
+
+        }
+    }
+}
